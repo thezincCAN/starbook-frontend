98b2fcd5f38aeeb693d1b29807c380e760ead207
Amanda Michelle Hecht <amandahecht89@gmail.com>
2023-01-16 13:41:48 -0700

    started title page added nav links

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import {
 
 function App() {
   return (
-    <div className="App">
-      <div className='coverpage'>
+    <Router>
+      <div className="App">
+        <div className='coverpage'>
+          <h1>STAR BOOK TITLE</h1>
+          <h4>Written By:<br></br>Amanda Hecht<br></br>Chad Stabler<br></br>Niki Hite</h4>
+          <div>IMAGE HERE</div>
+          <br></br><br></br><br></br><br></br>
+          <nav>
+            <Link className='navLink' to='/story'>Story</Link><br></br>
+            <Link className='navLink' to='/credits'>Authors</Link>
+          </nav>
 
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
