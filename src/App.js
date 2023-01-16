0acd559f1e81bf05ca8ab8395f0bba35a8e552c9
Chad Stabler <stablerpsn@gmail.com>
2023-01-16 15:52:26 -0600

    initial page components created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import './App.css';
 import {
   BrowserRouter as Router,
-  Routes,
   Route,
   Link,
 } from 'react-router-dom';
+import Credits from './Pages/Credits';
+import Story from './Pages/Story';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <div className='coverpage'>
-          <h1>STAR BOOK TITLE</h1>
-          <h4>Written By:<br></br>Amanda Hecht<br></br>Chad Stabler<br></br>Niki Hite</h4>
-          <div>IMAGE HERE</div>
-          <br></br><br></br><br></br><br></br>
-          <nav>
-            <Link className='navLink' to='/story'>Story</Link><br></br>
-            <Link className='navLink' to='/credits'>Authors</Link>
-          </nav>
+      <Route exact path='/'>
+        <div className="App">
+          <div className='coverpage'>
+            <h1>STAR BOOK TITLE</h1>
+            <h4>Written By:<br></br>Amanda Hecht<br></br>Chad Stabler<br></br>Niki Hite</h4>
+            <div>IMAGE HERE</div>
+            <br></br><br></br><br></br><br></br>
+            <nav>
+              <Link className='navLink' to='/story'>Story</Link><br></br>
+              <Link className='navLink' to='/credits'>Authors</Link>
+            </nav>
 
+          </div>
         </div>
-      </div>
+      </Route>
+      <Route exact path='/credits'>
+        <Credits />
+      </Route>
+      <Route exact path='/story'>
+        <Story />
+      </Route>
     </Router>
   );
 }
