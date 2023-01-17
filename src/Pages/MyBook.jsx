b175b43245cf6c049287c099021b57a0e35dfaa1
Niki Hite <nikihite@comcast.net>
2023-01-17 10:34:25 -0800

    added book styling

--- src/Pages/MyBook.jsx
+++ src/Pages/MyBook.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import HTMLFlipBook from 'react-pageflip';
+import './BookStyling.css';
 
 export default function MyBook() {
   return <div>
@@ -7,6 +7,30 @@ export default function MyBook() {
       <NavLink to='/'>Home</NavLink>
       <NavLink to='/credits'>Authors</NavLink>
     </header>
-    Magical story stuff here
+
+    <div id="wrapper">
+      <div id="container">
+
+        <section className="open-book">
+          <header>
+            <h1>Star Book</h1>
+          </header>
+          <article>
+            <dl>
+              THIS IS OUR PARAGRAPH
+            </dl>
+
+          </article>
+          <footer>
+            <ol id="page-numbers">
+              <li>1</li>
+              <li>2</li>
+            </ol>
+          </footer>
+        </section>
+
+      </div>
+    </div>
+
   </div>;
 }
